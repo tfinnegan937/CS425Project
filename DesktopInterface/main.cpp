a8644272b7761354f1e49934d0f8de1e228e1fa7
Tim Finnegan <FinneganTimothyD@Gmail.com>
2021-01-26 12:11:41 -0800

    Added shared IPC library to build chain

--- DesktopInterface/main.cpp
+++ DesktopInterface/main.cpp
@@ -1,8 +1,6 @@
 #include <QApplication>
 #include "DesktopInterface.h"
-#include "UnrealIPCController.h"
 int main(int argc, char * argv[]) {
-    instantiate_shared_mem("test");
     QApplication app (argc, argv);
     DesktopInterface application(&app);
     return application.run();
