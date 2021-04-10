e130d04e194974d0a3ebc23a57c657e8db732020
Tim Finnegan <FinneganTimothyD@Gmail.com>
2021-04-10 15:11:06 -0700

    THE UI NOW WORKS!

--- Intermediate/Build/Win64/CS425ProjectEditor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.RTTI.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29334
-#include "E:/CS425Project/Intermediate/Build/Win64/CS425ProjectEditor/DebugGame/Engine/SharedPCH.Engine.NonOptimized.RTTI.ShadowErrors.h"
--- Intermediate/Build/Win64/CS425ProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29334
-#include "E:/CS425Project/Intermediate/Build/Win64/CS425ProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.ShadowErrors.h"
